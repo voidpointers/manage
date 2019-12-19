4ba2fc48361dfd10e4e39e05fe187a8c74f78456
bryan <wangyabener@qq.com>
2019-12-19 15:21:05 +0800

    title attribute

--- app/Receipt/Entities/Transaction.php
+++ app/Receipt/Entities/Transaction.php
@@ -7,4 +7,15 @@ use App\Model;
 class Transaction extends Model
 {
     protected $table = 'receipt_transactions';
+
+    /**
+     * Get the transaction's title.
+     *
+     * @return string
+     */
+    public function getTitleAttribute()
+    {
+        $attributes = json_decode($this->attributes['attributes'], true) ?? [];
+        return implode(' - ', array_values($attributes));
+    }
 }
