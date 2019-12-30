9358bab7aa3129ed918075ddc1887ef0682d4610
bryan <wangyabener@qq.com>
2019-12-30 14:03:47 +0800

    增加排序

--- database/migrations/2019_12_20_065618_create_logistics_providers_table.php
+++ database/migrations/2019_12_20_065618_create_logistics_providers_table.php
@@ -19,6 +19,7 @@ class CreateLogisticsProvidersTable extends Migration
             $table->string('title', 64)->default('')->comment('物流商');
             $table->string('en')->default('')->comment('英文或拼音名');
             $table->string('code', 32)->default('')->comment('代号');
+            $table->mediumInteger('sort')->default(0)->comment('排序');
         });
 
         DB::statement("ALTER TABLE `logistics_providers` comment '物流商'"); // 表注释
