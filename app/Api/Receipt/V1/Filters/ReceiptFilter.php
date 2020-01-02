9accb53689e34f6e41a1c2cea3f46fc48838a871
bryan <wangyabener@qq.com>
2020-01-02 20:11:49 +0800

    增加buyer user id 搜索

--- app/Api/Receipt/V1/Filters/ReceiptFilter.php
+++ app/Api/Receipt/V1/Filters/ReceiptFilter.php
@@ -22,6 +22,9 @@ class ReceiptFilter
         if ($request->has('etsy_receipt_id')) {
             $this->query->where('etsy_receipt_id', $request->get('etsy_receipt_id'));
         }
+        if ($request->has('buyer_user_id')) {
+            $this->query->where('buyer_user_id', $request->get('buyer_user_id'));
+        }
         if ($request->has('creation_tsz_start') || $request->has('creation_tsz_end')) {
             $this->query->creationTsz([
                 $request->get('creation_tsz_start'),
