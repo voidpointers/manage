8f5f088c6c1bcb77bc66bf5265572d76ee6cbff8
bryan <wangyabener@qq.com>
2020-01-06 14:38:00 +0800

    fix bug

--- database/migrations/2019_12_20_065618_create_logistics_providers_table.php
+++ database/migrations/2019_12_20_065618_create_logistics_providers_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class CreateExpresssTable extends Migration
+class CreateLogisticsProvidersTable extends Migration
 {
     /**
      * Run the migrations.
