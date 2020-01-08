bb931626f9ed87dd4ddc80c5d5ea4da185250454
bryan <wangyabener@qq.com>
2020-01-08 11:46:41 +0800

    fix

--- app/Express/Services/ExpressService.php
+++ app/Express/Services/ExpressService.php
@@ -62,28 +62,33 @@ class ExpressService
     protected function buildOrders($packages, $channel)
     {
         $orders = [];
-
         foreach ($packages as $package) {
-            $orders[] = [
-                'CustomerOrderNumber' => $package->package_sn,
-                'ShippingMethodCode' => $channel,
-                'PackageCount' => 1,
-                'Weight' => '',
-                'Receiver' => [
-                    'CountryCode' => $package->consignee->country_code,
-                    'FirtstName' => $package->consignee->name,
-                    'LastName' => 'Rest',
-                    'Street' => str_replace('&#39,', '', 
-                        $package->consignee->first_line . $package->consignee->second_line
-                    ),
-                    'City' => $package->consignee->city,
-                    'State' => $package->consignee->state,
-                    'Zip' => $package->consignee->zip,
-                    'Phone' => $package->consignee->phone,
-                ],
-            ];
+            $orders[] = $this->build($package, $channel);
         }
+        return $orders;
+    }
 
+    protected function build($package, $channel)
+    {
+        $orders = [
+            'CustomerOrderNumber' => $package->package_sn,
+            'ShippingMethodCode' => $channel,
+            'PackageCount' => 1,
+            'Weight' => '',
+            'Receiver' => [
+                'CountryCode' => $package->consignee->country_code,
+                'FirtstName' => $package->consignee->name,
+                'LastName' => 'Rest',
+                'Street' => str_replace('&#39,', '', 
+                    $package->consignee->first_line . $package->consignee->second_line
+                ),
+                'City' => $package->consignee->city,
+                'State' => $package->consignee->state,
+                'Zip' => $package->consignee->zip,
+                'Phone' => $package->consignee->phone,
+            ],
+            
+        ];
         $total_weight = 0;
         $parcels = [];
         foreach ($package->item as $item) {
@@ -100,7 +105,6 @@ class ExpressService
         }
         $orders['Weight'] = $total_weight;
         $orders['Parcels'] = $parcels;
-
         return $orders;
     }
 }
