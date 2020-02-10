a30387d5de21ade0077c99878adbd5311198e3fc
bryan <wangyabener@qq.com>
2020-02-10 14:04:39 +0800

    增加授权

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => User\Entities\User::class,
+            'model' => App\User::class,
         ],
 
         // 'users' => [
